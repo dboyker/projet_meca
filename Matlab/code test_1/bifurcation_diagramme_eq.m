a723a198ffadd5b4836afb716d4c9b55155892de
David Boyker <dboyker@gmail.com>
2015-12-14 16:35:57 +0100

    merging

--- Matlab/code test_1/bifurcation_diagramme_eq.m
+++
@@ -1,9 +0,0 @@
-function dy = bifurcation_diagramme_eq(t,y)
-
-global c w k
-
-dy = zeros(2,1);   
-
-dy(1) = y(2);
-
-dy(2) = -k*y(2)-sin(y(1))+c*cos(w*t);
--- Matlab/main.m
+++ Matlab/main.m
@@ -4,40 +4,34 @@
 % BLEU = PENDULE 2 (condition initiale légèrement différente => maybe chaos
 % VERT = PENDULE 3 idem pendule si besoin (bifurcation)
 
-global k k1 k2 m l l1 l2 Longueur l0 A omega x0 x1 x2 time
-m=5;k1=5;k2=10;l=10;k=0;A=500;omega=2;l1=l/2;l2=l/2;
+global k k1 k2 m l l1 l2 A omega x0 x1 x2 time
+m=5;k1=5;k2=10;l=10;k=0;A=230;omega=1;l1=l/2;l2=l/2;
+
 %%% Conditions Initiales %%%
 % note: x = [theta, theta0.]
-x0 = [1.708 0];
+x0 = [1.71 0];
 x1 = [1.7 0];
-x2 = [1.1508 0];
-
-%x0 = [0 0];
-%x1 = [0 0.1];
-%x2 = [0 0.002];
-
+x2 = [1.69 0];
 
 
 %%% DIAGRAMME DE BASE %%%
 time = [0:0.2:60];
 %diagramme_base()
 
+
 %%% DIAGRAMME DE PHASE %%%
-time = [0:0.25:40];
-
-    diagramme_phase()
-
-
+time = [0:0.25:60];
+%diagramme_phase()
 
 
 %%% SECTION POINCARE %%%
 %time = [0:2*pi/omega:10000]
-time = [0:200];
-%sectionpoincare(2*pi/omega)
-
+time = [0:1:1000];
+%sectionpoincare()
 
 
 %%% DIAGRAMME BIFURCATION %%%
-m=5;k1=3;k2=5;l=5;k=0;A=100;omega=2;l1=l/2;l2=l/2;
-time=[100:100:500];
+time=[0:50:500];
 %diagramme_bifurcation()
+%dia_bifurc_test_omega()
+dia_bifurc_test_A()
